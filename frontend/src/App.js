224843f0b1c00963261253189d3af1075b99d63a
Vikram Ajith <vikiajith99@gmail.com>
2025-03-19 21:29:06 +0530

    uploaded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,37 +4,17 @@ import MyJourney from "./MyJourney";
 import JourneyExperience from "./JourneyExperience";
 import Chatbot from "./Chatbot";
 import LiveUpdates from "./LiveUpdates";
-import Login from "./Login";
-import { useStateValue } from "./StateProvider";
-//import { StateProvider } from "./StateProvider";
-import Chat from "./Chat";
-import Sidebar from "./Sidebar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [{ user }] = useStateValue(); // Ensure `useStateValue` is correctly imported
-
-  return (
-    <BrowserRouter> {/* ✅ Moved BrowserRouter here */}
-      <div className="App">
-        {!user ? (
-          <Login />
-        ) : (
-          <div className="app__body">
-            {/* <Sidebar /> */}
+return (
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/my-journey" element={<MyJourney />} />
               <Route path="/journey-experience" element={<JourneyExperience />} />
               <Route path="/chatbot" element={<Chatbot />} />
-              <Route path="/live-updates" element={<LiveUpdates />} />
-              <Route path="/rooms/:roomId" element={<Chat />} />
+              <Route path="/live-updates" element={<LiveUpdates />} />          
             </Routes>
-          </div>
-        )}
-      </div>
-    </BrowserRouter>
   );
 }
-
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import HomePage from './HomePage';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import MyJourney from './MyJourney';
-import JourneyExperience from './JourneyExperience';
-import Chatbot from './Chatbot';
-import LiveUpdates from './LiveUpdates';
-import reducer, { initialState } from "./reducer";
-import { StateProvider } from "./StateProvider";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <Router>
-    <App />
-    {/* <HomePage/>
-    <MyJourney/>
-    <JourneyExperience/>
-    <Chatbot/>
-    <LiveUpdates/> */}
-  </Router>
-   
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
-
