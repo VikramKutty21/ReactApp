f32ca5244531ea5f7067ad1381c15cec4c4477a9
Vikram Ajith <vikiajith99@gmail.com>
2025-03-19 11:14:24 +0530

    uploaded

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,13 +9,16 @@ import MyJourney from './MyJourney';
 import JourneyExperience from './JourneyExperience';
 import Chatbot from './Chatbot';
 import LiveUpdates from './LiveUpdates';
+import reducer, { initialState } from "./reducer";
+import { StateProvider } from "./StateProvider";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
     <App />
-    {/* <HomePage/> */}
-    {/* <MyJourney/>
+    {/* <HomePage/>
+    <MyJourney/>
     <JourneyExperience/>
     <Chatbot/>
     <LiveUpdates/> */}
@@ -24,8 +27,10 @@ root.render(
   </React.StrictMode>
 );
 
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
 
